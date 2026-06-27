b4f5ab813c6cd042fa37de33d3e6ab437f80c244
luiscab19 <luis19alvarez05@gmail.com>
2026-06-27 16:58:34 -0400

    feat:prueba de funcionamiento del login con back

--- cine-rate-front/src/main.ts
+++ cine-rate-front/src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { provideIonicAngular } from '@ionic/angular/standalone';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
@@ -10,6 +11,7 @@ bootstrapApplication(AppComponent, {
       mode: 'md',
       innerHTMLTemplatesEnabled: true,
     }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient()
   ]
 });
